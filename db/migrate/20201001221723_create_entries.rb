beb6913082164d2466d0a752bf85d32a979370ef
Borrison <borissoto@gmail.com>
2020-10-06 19:44:11 -0400

    adding a category model and its relation to entries

--- db/migrate/20201001221723_create_entries.rb
+++ db/migrate/20201001221723_create_entries.rb
@@ -2,7 +2,7 @@ class CreateEntries < ActiveRecord::Migration[6.0]
   def change
     create_table :entries do |t|
       t.string :meal_type
-      t.integer :calores
+      t.integer :calories
       t.integer :proteins
       t.integer :carbohydrates
       t.integer :fats
